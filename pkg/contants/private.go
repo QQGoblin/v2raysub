ff9c0dfe356899f2717054303b552fa87b38e9e6
LQing <linqing@ruijie.com.cn>
2023-04-03 17:26:30 +0800

    init

--- pkg/contants/private.go
+++ pkg/contants/private.go
@@ -2,17 +2,17 @@ package contants
 
 // TODO: 以下是敏感信息，不要上传仓库
 
-//const (
-//	DefaultID        = ""
-//	DefaultAlterId   = ""
-//	DefaultVMessPort = ""
-//
-//	DefaultSockPort = ""
-//	DefaultSockUser = ""
-//	DefaultSockPass = ""
-//
-//	Region   = ""
-//	Key      = ""
-//	Secret   = ""
-//	Endpoint = ""
-//)
+const (
+	DefaultID        = ""
+	DefaultAlterId   = ""
+	DefaultVMessPort = ""
+
+	DefaultSockPort = ""
+	DefaultSockUser = ""
+	DefaultSockPass = ""
+
+	Region   = "cn-hongkong"
+	Key      = ""
+	Secret   = ""
+	Endpoint = "ecs.cn-hangzhou.aliyuncs.com"
+)
